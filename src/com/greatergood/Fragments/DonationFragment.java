944718eb37499216250990afe2e27836039deec2
Khalid <kaziz@u.northwestern.edu>
2014-04-20 19:50:28 -0500

    Working slider text

--- src/com/greatergood/Fragments/DonationFragment.java
+++ src/com/greatergood/Fragments/DonationFragment.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
 import com.greatergood.R;
 
@@ -21,15 +23,48 @@ public class DonationFragment extends Fragment {
 
     }
 
+    private Button btnDonation;
+
+    private void setDonationButtonText(int dollarAmount) {
+        btnDonation.setText("Donate $" + Integer.toString(dollarAmount));
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_donation, container);
 
+        btnDonation = (Button) v.findViewById(R.id.donate_button);
+
         SeekBar sbSuggestedDonation = (SeekBar) v
                 .findViewById(R.id.donation_seek_bar);
 
-        sbSuggestedDonation.setProgress(5);
+        sbSuggestedDonation.setProgress(10);
+        setDonationButtonText(10);
+
+        sbSuggestedDonation
+                .setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                        // TODO Auto-generated method stub
+
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+                        // TODO Auto-generated method stub
+
+                    }
+
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar,
+                            int progress, boolean fromUser) {
+                        // TODO Auto-generated method stub
+                        setDonationButtonText(progress);
+
+                    }
+                });
 
         return super.onCreateView(inflater, container, savedInstanceState);
     }
