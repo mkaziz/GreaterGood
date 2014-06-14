034a6f0a715946f79494cb99b1e62a072b12e43d
Khalid <kaziz@u.northwestern.edu>
2014-06-14 00:36:15 -0500

    Still broken but minor progress

--- src/com/greatergood/BusinessObjects/GoogleApiClientManager.java
+++ src/com/greatergood/BusinessObjects/GoogleApiClientManager.java
@@ -3,15 +3,20 @@ package com.greatergood.BusinessObjects;
 import android.content.Context;
 import android.os.Bundle;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.plus.Plus;
+import com.greatergood.fragments.SignInFragment;
 
-public class GoogleApiClientManager implements ConnectionCallbacks {
+public class GoogleApiClientManager implements ConnectionCallbacks,
+        OnConnectionFailedListener {
     private GoogleApiClient mGoogleApiClient;
 
-    public static GoogleApiClientManager get(Context c) {
+    public static GoogleApiClientManager get(Context c, SignInFragment f) {
         GoogleApiClientManager gacm = new GoogleApiClientManager();
-        // gacm.Builder(c);
+        gacm.load(c, f);
         return gacm;
     }
 
@@ -35,14 +40,6 @@ public class GoogleApiClientManager implements ConnectionCallbacks {
         return mGoogleApiClient.isConnecting();
     }
 
-    public GoogleApiClientManager load(Context c) {
-        // mGoogleApiClient = new GoogleApiClient.Builder(c)
-        // .addConnectionCallbacks(this);
-        // .addOnConnectionFailedListener(this).addApi(Plus.API)
-        // .addScope(Plus.SCOPE_PLUS_PROFILE).build();
-        return this;
-    }
-
     @Override
     public void onConnected(Bundle connectionHint) {
         // TODO Auto-generated method stub
@@ -52,6 +49,25 @@ public class GoogleApiClientManager implements ConnectionCallbacks {
     @Override
     public void onConnectionSuspended(int cause) {
         // TODO Auto-generated method stub
+        connect();
+    }
 
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // TODO Auto-generated method stub
+        // if (!result.hasResolution()) {
+        // GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(),
+        // mContext, 0).show();
+        // return;
+        // }
+    }
+
+    private void load(Context c, SignInFragment f) {
+        mGoogleApiClient = new GoogleApiClient.Builder(c)
+                .addConnectionCallbacks(f).addOnConnectionFailedListener(f)
+                .addApi(Plus.API).addScope(Plus.SCOPE_PLUS_PROFILE).build();
+        // .addOnConnectionFailedListener(this).addApi(Plus.API)
+        // .addScope(Plus.SCOPE_PLUS_PROFILE).build();
+        // mContext = c;
     }
 }
