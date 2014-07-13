deee98d7b30e6f60729c05a61a77efb016292114
Khalid <kaziz@u.northwestern.edu>
2014-07-12 22:52:36 -0500

    Linked PreferencesFragment signin button to SignInActivity

--- src/com/greatergood/fragments/PreferencesFragment.java
+++ src/com/greatergood/fragments/PreferencesFragment.java
@@ -1,10 +1,12 @@
 package com.greatergood.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 
 import com.greatergood.R;
+import com.greatergood.activities.SignInActivity;
 
 public class PreferencesFragment extends PreferenceFragment {
 
@@ -16,12 +18,18 @@ public class PreferencesFragment extends PreferenceFragment {
         addPreferencesFromResource(R.xml.preferences);
 
         Preference signInPreference = findPreference("pref_sign_in");
+
         // GoogleApiClientManager.get(getActivity(), this, this);
         signInPreference
                 .setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
                     @Override
                     public boolean onPreferenceClick(Preference arg0) {
                         // code for what you want it to do
+
+                        Intent i = new Intent(getActivity(),
+                                SignInActivity.class);
+                        startActivity(i);
+
                         return true;
                     }
                 });
